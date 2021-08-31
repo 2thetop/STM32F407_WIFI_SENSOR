93da9694e3c3fc85158ae538b14646725e209a40
Jang Suk Jung <2thetop@gmail.com>
2021-08-31 21:39:52 +0900

    ESP_nRESET 핀에 Pullup을 제거함.

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -69,7 +69,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = ESP_nRESET_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;	//GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(ESP_nRESET_GPIO_Port, &GPIO_InitStruct);
 
